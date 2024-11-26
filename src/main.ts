201bc02b7fd6455743307c35e359611ef82a590a
SuperBallista <140693798+SuperBallista@users.noreply.github.com>
2024-11-27 03:21:32 +0900

    update bug

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,17 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
-    console.log(process.env.DB_PORT);
-  console.log(process.env.DB_HOST);
-  console.log(process.env.DB_USER);
-  console.log(process.env.DB_PASSWORD);
-  console.log(process.env.DB_NAME); 
-
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: 'http://localhost:5000', // 클라이언트 도메인
     credentials: true, // 쿠키 포함 허용
   });
+  console.log(process.env.DB_PORT);
+  console.log(process.env.DB_HOST);
+  console.log(process.env.DB_USER);
+  console.log(process.env.DB_PASSWORD);
+  console.log(process.env.DB_NAME); 
 
   await app.listen(process.env.WEB_PORT);
 }
