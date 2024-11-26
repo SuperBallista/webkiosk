365c29e0358bedf4e96aea096cf8112eb59ff8b7
SuperBallista <140693798+SuperBallista@users.noreply.github.com>
2024-11-27 03:11:45 +0900

    로그 위치 변경

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,18 @@ import { AppModule } from './app.module';
 
 
 async function bootstrap() {
+    console.log(process.env.DB_PORT);
+  console.log(process.env.DB_HOST);
+  console.log(process.env.DB_USER);
+  console.log(process.env.DB_PASSWORD);
+  console.log(process.env.DB_NAME); 
+
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: 'http://localhost:5000', // 클라이언트 도메인
     credentials: true, // 쿠키 포함 허용
   });
-  console.log(process.env.DB_PORT);
-  console.log(process.env.DB_HOST);
-  console.log(process.env.DB_USER);
-  console.log(process.env.DB_PASSWORD);
-  console.log(process.env.DB_NAME); 
 
   await app.listen(process.env.WEB_PORT);
 }
