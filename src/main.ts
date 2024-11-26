6604b90362dc12527fce56274a61ced87b309a70
SuperBallista <140693798+SuperBallista@users.noreply.github.com>
2024-11-27 05:44:46 +0900

    server update

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const configService = app.get(ConfigService);
   app.enableCors({
-    origin: 'http://localhost:5000', // 클라이언트 도메인
+    origin: configService.get<string>('REDIRECT_URL'), // 클라이언트 도메인
     credentials: true, // 쿠키 포함 허용
   });
 
